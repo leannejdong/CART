aa6454a6312b0925809ca248d4acdf10479f87ac
Leanne Dong <jdleanne@gmail.com>
2020-10-13 23:00:25 -0400

    saved model is correct, but printout looks strange

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "train_test.h"
 #include "DecisionTree.h"
-int main()
-{
+int main() {
     int ret = test_decision_tree_train();
 
     if (ret == 0) fprintf(stdout, "========== test success ==========\n");
-    else fprintf(stderr, "########## test fail ##########\n");
+    else
+        fprintf(stderr, "########## test fail ##########\n");
+
+    int ret1 = test_decision_tree_predict();
+
+    if (ret1 == 0) fprintf(stdout, "========== test success ==========\n");
+    else
+        fprintf(stderr, "########## test fail ##########\n");
 
     return 0;
 }
--- train_test.h
+++ train_test.h
@@ -7,36 +7,35 @@
 
 template int read_txt_file<float>(std::string_view, std::vector<std::vector<float>>&, const char, const int, const int);
 
-int test_decision_tree_train()
-{
+int test_decision_tree_train() {
     // small dataset test
-    const std::vector<std::vector<float>> data{ { 2.771244718f, 1.784783929f, 0.f },
-                                                { 1.728571309f, 1.169761413f, 0.f },
-                                                { 3.678319846f, 2.81281357f, 0.f },
-                                                { 3.961043357f, 2.61995032f, 0.f },
-                                                { 2.999208922f, 2.209014212f, 0.f },
-                                                { 7.497545867f, 3.162953546f, 1.f },
-                                                { 9.00220326f, 3.339047188f, 1.f },
-                                                { 7.444542326f, 0.476683375f, 1.f },
-                                                { 10.12493903f, 3.234550982f, 1.f },
-                                               {6.642287351f, 3.319983761f, 1.f}};
-    const std::vector<float> classes{0.f, 1.f};
-    ANN::DecisionTree<float> dt;
-    dt.init(data, classes);
-    dt.set_max_depth(3);
-    dt.set_min_size(1);
-    dt.train();
-
-    std::string_view model_name = "/home/leanne/CLionProjects/CART/saved_model/decision_tree.model";
-    dt.save_model(model_name);
-    //    ANN::DecisionTree<float> dt2;
-    dt.load_model(model_name);
-    // fix it!
-    //    const std::vector<std::vector<float>> test{{0.6f, 1.9f, 0.f}, {9.7f, 4.3f, 1.f}};
-    //    for (const auto& row : test) {
-    //        float ret = dt.predict(row);
-    //        fprintf(stdout, "predict result: %.1f, actural value: %.1f\n", ret, row[2]);
-    //    }
+    //    const std::vector<std::vector<float>> data{ { 2.771244718f, 1.784783929f, 0.f },
+    //                                                { 1.728571309f, 1.169761413f, 0.f },
+    //                                                { 3.678319846f, 2.81281357f, 0.f },
+    //                                                { 3.961043357f, 2.61995032f, 0.f },
+    //                                                { 2.999208922f, 2.209014212f, 0.f },
+    //                                                { 7.497545867f, 3.162953546f, 1.f },
+    //                                                { 9.00220326f, 3.339047188f, 1.f },
+    //                                                { 7.444542326f, 0.476683375f, 1.f },
+    //                                                { 10.12493903f, 3.234550982f, 1.f },
+    //                                               {6.642287351f, 3.319983761f, 1.f}};
+    //    const std::vector<float> classes{0.f, 1.f};
+    //    ANN::DecisionTree<float> dt;
+    //    dt.init(data, classes);
+    //    dt.set_max_depth(3);
+    //    dt.set_min_size(1);
+    //    dt.train();
+    //
+    //    std::string_view model_name = "/home/leanne/CLionProjects/CART/saved_model/decision_tree.model";
+    //    dt.save_model(model_name);
+    //    //    ANN::DecisionTree<float> dt2;
+    //    dt.load_model(model_name);
+    //    // fix it!
+    //        const std::vector<std::vector<float>> test{{0.6f, 1.9f, 0.f}, {9.7f, 4.3f, 1.f}};
+    //        for (const auto& row : test) {
+    //            float ret = dt.predict(row);
+    //            fprintf(stdout, "predict result: %.1f, actural value: %.1f\n", ret, row[1]);
+    //        }
 
     //    // banknote authentication dataset
     std::string_view file_name = "/home/leanne/CLionProjects/CART/banknote_authentication.txt";
@@ -57,7 +56,7 @@ int test_decision_tree_train()
     dt1.set_min_size(10);
     dt1.train();
 
-    std::string_view model_name1 = "/home/leanne/CLionProjects/CART/saved_model/decision_tree1.model";
+    std::string_view model_name1 = "/home/leanne/CLionProjects/CART/saved_model/decision_tree.model";
     dt1.save_model(model_name1);
 
     return 0;
@@ -65,7 +64,7 @@ int test_decision_tree_train()
 
 int test_decision_tree_predict()
 {
-    std::string_view model_name = "/home/leanne/CLionProjects/CART/saved_model/decision_tree_test.model";
+    std::string_view model_name = "/home/leanne/CLionProjects/CART/saved_model/decision_tree.model";
     ANN::DecisionTree<float> dt;
     dt.load_model(model_name);
     int max_depth = dt.get_max_depth();
@@ -78,8 +77,8 @@ int test_decision_tree_predict()
                                           {-1.0401,9.3987,0.85998,-5.3336,0},
                                           {1.0637,3.6957,-4.1594,-1.9379,1}};
     for (const auto& row : test) {
-        float ret = dt.predict(row);
-        fprintf(stdout, "predict result: %.1f, actual value: %.1f\n", ret, row[4]);
+        float ret_predict = dt.predict(row);
+        fprintf(stdout, "predict result: %.1f, actual value: %.1f\n", ret_predict, row[4]);
     }
 
     return 0;
